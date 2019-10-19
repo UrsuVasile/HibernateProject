dc0bef9d9b6409ac5a2f017958de248c69ba4f98
vasile <vsl_ursu@yahoo.com>
2019-10-19 14:40:33 +0300

    - Created UserDao class

---
+++ src/main/java/Database/UserDao.java
@@ -0,0 +1,20 @@
+package Database;
+
+import Entity.User;
+
+public class UserDao extends DbInitializer{
+
+    public void insertUser(User user) {
+        openSessionAndTransaction();
+        session.persist(user);
+        closeSessionAndTransaction();
+    }
+
+    public User findUserById(int id) {
+        openSessionAndTransaction();
+        User user = session.find(User.class, id);
+        System.out.println(user);
+        closeSessionAndTransaction();
+        return user;
+    }
+}
